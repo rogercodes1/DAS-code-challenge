80d850c0de63bbe1be536a88d4779e9691a96db0
rogercodes1 <rogerperez.us@gmail.com>
2018-09-06 10:42:20 -0700

    trying something different.

--- appointments.js
+++ appointments.js
@@ -1,24 +1,43 @@
 const appointmentType = `http://localhost:3000/testdrive`
-const exactDay = `${appointmentType}/monday`
 
-function fetchAvailableDays() {
+async function fetchAvailableDays() {
   let daysArr=[];
-  fetch(appointmentType)
-  .then(response=>response.json())
-  .then(json=>{
-    json.forEach(day=>{
-      // debugger
-      daysArr.push(`${day.id}`)
+  let dayObjData = {}
+  let response = await fetch(appointmentType)
+  let data = await response.json()
+  await data.forEach(day=>{
+    // debugger
+    dayObjData[day.id] = day.times
+    daysArr.push(`${day.id}`)
     })
+    run()
     return daysArr
-  })
-
+  debugger
 }
-function fetchAvailableTimesByDay(days) {
-  days.forEach(day=>{
-    debugger;
-    fetch(`${appointmentType}/${day.toLowerCase()}`)
-  })
-}
+// function fetchAvailableTimesByDay(daysArr) {
+//   debugger;
+//   daysArr.forEach(day=>{
+//     debugger;
+//     fetch(`${appointmentType}/${day}`)
+//   })
+// }
  fetchAvailableDays()
 // fetchAvailableTimesByDay()
+// async function run(){
+//   let availableTimes;
+//   let arr = await fetchAvailableDays()
+//   await arr.forEach(day=>{
+//     let resp = await fetch(`${appointmentType}/${day}`)
+//     // debugger;
+//     let data = await resp.json()
+//     // .then(json=>{
+//     //   // debugger
+//     //   availableTimes.push(json.times)
+//     // })
+//
+//
+//   })
+//   debugger
+// }
+
+// run()
