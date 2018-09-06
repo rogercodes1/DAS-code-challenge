6aa7c57004a099b3a34266329dca35ada14f822d
rogercodes1 <rogerperez.us@gmail.com>
2018-09-06 09:31:08 -0700

    added json server and index file.

--- appointments.js
+++ appointments.js
@@ -1,7 +1,15 @@
-const daysEndpoint = `localhost:8001/test%20drive`
+const appointmentType = `localhost:3000/test%20drive`
 const exactDay = `localhost:8001/test%20drive/monday`
+let days;
 
-
-function fetchFromAPI() {
+function fetchAvailableDays() {
+  fetch(daysAvailable)
+  .then(res=>res.json())
+  .then(json=>{
+    debugger
+    return (days = json)
+  })
+}
+function fetchAvailableTimesByDay(days) {
 
 }
