57e2974b42e65bb31d412bc4060ac7a1ac09418b
rogercodes1 <rogerperez.us@gmail.com>
2018-09-07 13:02:37 -0700

    added second alternate.

---
+++ sec-alternate.js
@@ -0,0 +1,62 @@
+// install json-server
+// npm install -g json-server
+//Run server
+// json-server --watch daysAPI.json
+
+const appointmentType = `http://localhost:3000/testdrive`
+
+let fetchDays = ()=>{
+  return new Promise((resolve) =>{
+    fetch(appointmentType)
+    .then(res=>res.json())
+    .then(json=>resolve(json))
+    .catch(err => {console.log('what is err',err)})
+  })
+}
+
+ const fetchAvailTimesByDay = (day)=> {
+  return new Promise((resolve,reject)=>{
+      fetch(`${appointmentType}/${day[0].id}`)
+      .then(res=>{
+        if (res.ok){
+          res.json()
+          .then(json=>{resolve(json.times)})
+        }
+        else {
+          reject(console.log("what is res.json()", res))
+        }
+      })
+  })
+  }
+
+
+const finalPromises = async ()=>{
+  Promise.all([fetchDays(), fetchAllTimesByDay()])
+  .then(values=>{
+    debugger
+  })
+  console.log(first);
+  console.log(oneDay);
+  console.log(multiDays);
+  return oneDay
+}
+
+const fetchAllTimesByDay = async(days)=> {
+  console.log(days);
+  let daysArr = []
+ return days.forEach(day=>{
+   fetch(`${appointmentType}/${day.id}`)
+   .then(res=>{
+     if (res.ok){
+       res.json()
+       .then(json=>{daysArr.push(json.times)})
+     }
+     else {
+       console.log("what is res.json()", res)
+     }
+   })
+   return daysArr
+   })
+ }
+
+console.log(finalPromises());
